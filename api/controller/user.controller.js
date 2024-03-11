f82742d2c4abd95cf531e227508852879bbe4ca6
Allah wasaya <awasaya058@gmail.com>
2024-03-11 08:10:48 +0500

    create get user listings api route

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
 import User from '../models/user.model.js';
+import Listing from '../models/lsiting.model.js';
 
 export const test = (req , res) =>{
     res.json({
@@ -31,6 +32,7 @@ export const updateUser =async (req , res , next) => {
         next(error);
     }
 };
+
 export const deleteUser = async (req , res , next) => {
     if(req.user.id !== req.params.id) return next(errorHandler(401 , 'You can only delete your own account'))
     try {
@@ -40,4 +42,18 @@ export const deleteUser = async (req , res , next) => {
     } catch (error) {
         next(error);
     }
-}
+};
+
+export const getUserListings = async(req, res, next) => {
+    if (req.user.id === req.params.id) {
+        try {
+            const listings = await Listing.find({ userRef : req.params.id});
+            res.status(200).json(listings);
+        } catch (error) {
+            next(error);
+        }
+    }else{
+        return next(errorHandler(401 , 'You canonly view your own listings!'));
+    }
+ 
+};
