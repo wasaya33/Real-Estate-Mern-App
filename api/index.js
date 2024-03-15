d0754249fd17e797f4ef40b9fb83cf750d4b0dd0
Allah wasaya <awasaya058@gmail.com>
2024-03-15 10:27:48 +0500

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,13 +5,15 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js'
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB');
 }).catch((err) => {
-    console.log(err)
+    console.log(err);
 })
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -24,6 +26,12 @@ app.listen(3000 , () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 
 app.use((err , req , res , next) => {
